a0751388b85a8a96985b22ccc35492192792be4f
GTornero <guilletorni@gmail.com>
2022-06-14 14:18:23 +0200

    edit sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = '0.0.1'
 extensions = [
     "myst_parser",
     "sphinx.ext.duration",
-    #"sphinx.ext.autosectionlabel",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
