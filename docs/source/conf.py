83073443e2714870335ca16c2fa4ac942b03ba67
GTornero <guilletorni@gmail.com>
2022-06-20 14:44:46 +0200

    Addint LayOWt logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,9 @@ exclude_patterns = ["_static"]
 #
 html_theme = 'furo'
 
+html_logo = '../../img/logo.png'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
